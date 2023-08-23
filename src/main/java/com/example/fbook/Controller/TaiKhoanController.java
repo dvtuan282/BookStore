59b2f5ca8b69c4231bd32b7b63d94635afaefc9d
dvtuan282 <dvtuan282@gmail.com>
2023-08-23 17:36:06 +0700

    update taiKhoan

--- src/main/java/com/example/fbook/Controller/TaiKhoanController.java
+++ src/main/java/com/example/fbook/Controller/TaiKhoanController.java
@@ -2,12 +2,12 @@ package com.example.fbook.Controller;
 
 import com.example.fbook.Entity.TaiKhoanEntity;
 import com.example.fbook.Service.ServiceImpl.TaiKhoanServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/K-Book")
@@ -18,6 +18,36 @@ public class TaiKhoanController {
     @GetMapping("/dang-ky")
     public String dangKy(Model model) {
         model.addAttribute("taiKhoan", new TaiKhoanEntity());
-        return "DangKy";
+        return "register";
+    }
+
+    @PostMapping("/dang-ky")
+    public String dangKy(@Valid @ModelAttribute("taiKhoan") TaiKhoanEntity taiKhoan,
+                         BindingResult result, Model model,
+                         @RequestParam(name = "ho") String ho,
+                         @RequestParam(name = "ten") String ten,
+                         @RequestParam(name = "matKhau") String matKhau) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        taiKhoan.setHoTen(ho + " " + ten);
+        taiKhoanService.dangKyTaiKhoan(taiKhoan);
+        return "redirect:/K-Book/dang-nhap";
+    }
+
+    @GetMapping("/dang-nhap")
+    public String formDangNhap(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/dang-nhap")
+    public String dangNhap(Model model,
+                           @RequestParam("email") String emial,
+                           @RequestParam("matKhau") String matKhau) {
+        TaiKhoanEntity taiKhoanEntity = taiKhoanService.dangNhap(emial, matKhau);
+        if (taiKhoanEntity == null) {
+            return "redirect:/K-Book/dang-nhap";
+        }
+        return "redirect:/K-Book/admin/trang-chu";
     }
 }
--- src/main/java/com/example/fbook/Entity/TaiKhoanEntity.java
+++ src/main/java/com/example/fbook/Entity/TaiKhoanEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Entity
@@ -11,18 +12,23 @@ import lombok.Data;
 @Data
 public class TaiKhoanEntity {
     @Id
+//    @NotEmpty(message = "Vui lòng nhập dữ liệu")
     @Column(name = "email")
     private String email;
 
+//    @NotEmpty(message = "Vui lòng nhập dữ liệu")
     @Column(name = "ho_ten")
     private String hoTen;
 
+//    @NotEmpty(message = "Vui lòng nhập dữ liệu")
     @Column(name = "mat_khau")
     private String matKhau;
 
+//    @NotEmpty(message = "Vui lòng nhập dữ liệu")
     @Column(name = "dia_chi")
     private String diaChi;
 
+//    @NotEmpty(message = "Vui lòng nhập dữ liệu")
     @Column(name = "so_dien_thoai")
     private String soDienThoai;
 
